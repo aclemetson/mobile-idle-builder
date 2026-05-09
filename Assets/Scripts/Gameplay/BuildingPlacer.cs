13ff96e03e5800aac3fd9ca18ac145cc89bdbb03
Anthony Clemetson <clemtek1@gmail.com>
2026-05-09 16:07:57 -0400

    chore(cleanup/track-7): checkpoint

--- Assets/Scripts/Gameplay/BuildingPlacer.cs
+++ Assets/Scripts/Gameplay/BuildingPlacer.cs
@@ -146,7 +146,6 @@ namespace MobileIdleBuilder
             if (fw > 1 || fh > 1)
                 _em.AddComponentData(entity, new BuildingFootprint { Width = fw, Height = fh });
 
-            // Write port layout
             if (hasPorts)
             {
                 var portBuf = _em.GetBuffer<PlacedPortData>(entity);
